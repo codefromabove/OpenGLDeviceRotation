a08212f17a58cb9fcba802228d1b8780a5528955
BarkyTheDog <philip@codefromabove.com>
2026-05-23 10:15:50 -0700

    UIScene adoption

--- OpenGLDeviceRotation/SceneDelegate.h
+++ OpenGLDeviceRotation/SceneDelegate.h
@@ -1,13 +1,14 @@
 //
-//  AppDelegate.h
+//  SceneDelegate.h
 //  OpenGLDeviceRotation
 //
-//  Created by Philip Schneider on 12/6/15.
 //  Copyright © 2015-2019 Philip Schneider. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
 
 @end
---
+++ OpenGLDeviceRotation/SceneDelegate.m
@@ -0,0 +1,36 @@
+//
+//  SceneDelegate.m
+//  OpenGLDeviceRotation
+//
+//  Copyright © 2015-2019 Philip Schneider. All rights reserved.
+//
+
+#import "SceneDelegate.h"
+
+@implementation SceneDelegate
+
+- (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions
+{
+}
+
+- (void)sceneDidDisconnect:(UIScene *)scene
+{
+}
+
+- (void)sceneDidBecomeActive:(UIScene *)scene
+{
+}
+
+- (void)sceneWillResignActive:(UIScene *)scene
+{
+}
+
+- (void)sceneWillEnterForeground:(UIScene *)scene
+{
+}
+
+- (void)sceneDidEnterBackground:(UIScene *)scene
+{
+}
+
+@end
